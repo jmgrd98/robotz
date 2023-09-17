abac698730a817a149faf612cacda415579a0bf4
João Marcelo Dantas <jmgrd98@gmail.com>
2023-09-17 11:29:34 -0300

    running bot by clicking button on frontend

--- app/api/run-bot/route.ts
+++ app/api/run-bot/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
+import { exec } from 'child_process';
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request, res: any) {
 
     try {
         if (req.method !== 'POST') {
@@ -12,6 +13,17 @@ export async function POST(req: Request, res: Response) {
         if (!botId) {
             return NextResponse.json(new Error('botId is required'), { status: 400 });
         }
+        await new Promise((resolve, reject) => {
+            exec('python C:\\Users\\Zello\\Documents\\Github\\robotz\\app\\bots\\tinder-bot\\tinder_bot.py', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject(new Error(`Error: ${error}`));
+                    return;
+                }
+                res.send(stdout);
+                resolve(stdout);
+            });
+        });
         return NextResponse.json({ message: `Bot with ID ${botId} has been started.`, botId, status: 200 });
     } catch (error) {
         console.log(error);
--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,31 @@ import { useState } from 'react';
 
 export default function Home() {
 
-  const [botId, setBotId] = useState('')
+  const [botId, setBotId] = useState('1')
 
   const runBot = async (e: any) => {
     e.preventDefault()
-    const res = await fetch('/api/run-bot')
-    const data = await res.json()
-    setBotId(data.id)
+    
+    try {
+      const res = await fetch('/api/run-bot', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ botId })
+      })
+      const data = await res.json()
+      console.log(data)
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-5xl font-bold'>Tinder Bot</h1>
       <button className='p-5 rounded bg-black text-white' onClick={runBot}>Rodar bot</button>
-      <div className='flex flex-col items-center'>
-        <h1 className='text-3xl font-bold'>Bot ID: {botId}</h1>
-      </div>
 
     </main>
   )
